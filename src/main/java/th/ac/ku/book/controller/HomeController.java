f2d19b07c9229d320430bd7310461603afd91231
nutchayalim <nutchaya.lim@ku.th>
2025-03-09 16:17:05 +0700

    edit word at controller

--- src/main/java/th/ac/ku/book/controller/HomeController.java
+++ src/main/java/th/ac/ku/book/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        model.addAttribute("greeting", "Hello 1.1");
+        model.addAttribute("greeting", "hi ja");
         // return home.html
         return "home";
     }
